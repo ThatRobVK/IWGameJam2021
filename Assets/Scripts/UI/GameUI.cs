4ad94563d96269fa9b861c4146555cd299aec8a0
Robert van Kooten <39274174+ThatRobVK@users.noreply.github.com>
2021-04-20 21:16:41 +0100

    Add player images to UI which move towards the centre with position

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -13,15 +13,30 @@ namespace FDaaGF.UI
         private Text[] opponentNames;
         [SerializeField]
         private ResourcesPanel[] resourcesPanels;
+        [SerializeField]
+        private Image playerImage;
+        [SerializeField]
+        private Image[] opponentImages;
+
+
+        private Vector2[] playerPositionVectors = new Vector2[] { new Vector2(0, -120), new Vector2(0, -100), new Vector2(0, -80), new Vector2(0, -60), new Vector2(0, -40), new Vector2(0, -20), new Vector2(0, 0) };
+        private Vector2[][] opponentPositionVectors = new Vector2[][] {
+             new Vector2[] { new Vector2(0, 120), new Vector2(0, 100), new Vector2(0, 80), new Vector2(0, 60), new Vector2(0, 40), new Vector2(0, 20), new Vector2(0,0) },
+             new Vector2[] { new Vector2(-120, 0), new Vector2(-100, 0), new Vector2(-80, 0), new Vector2(-60, 0), new Vector2(-40, 0), new Vector2(-20, 0), new Vector2(0,0) },
+             new Vector2[] { new Vector2(120, 0), new Vector2(100, 0), new Vector2(80, 0), new Vector2(60, 0), new Vector2(40, 0), new Vector2(20, 0), new Vector2(0,0) }
+        };
+
 
 
         public void UpdateUI(GameState gameState)
         {
             foreach (var player in gameState.Players)
             {
+                // Set player and opponent names in UI
                 var opponentNames = gameState.Players.Where(x => x.ConnectionId != player.ConnectionId).Select(x => x.Name).ToArray();
                 RpcSetNames(player.Connection, player.Name, opponentNames);
 
+                // Set resource counts
                 var resources = player.Resources.Select(x => x.Value).ToList();
                 var opponents = gameState.Players.Where(x => x.ConnectionId != player.ConnectionId).ToList();
                 foreach (var opponent in opponents)
@@ -29,6 +44,10 @@ namespace FDaaGF.UI
                     resources.AddRange(opponent.Resources.Select(x => x.Value).ToArray());
                 }
                 RpcSetResources(player.Connection, resources.ToArray());
+
+                // Set player positions
+                var opponentPositions = gameState.Players.Where(x => x.ConnectionId != player.ConnectionId).Select(x => x.Position).ToArray();
+                RpcSetPositions(player.Connection, player.Position, opponentPositions);
             }
         }
 
@@ -63,6 +82,24 @@ namespace FDaaGF.UI
                 for (int i = resources.Length + 3; i < 16; i += 4)
                 {
                     resourcesPanels[i / 4].gameObject.SetActive(false);
+                }
+            }
+        }
+
+        [TargetRpc]
+        private void RpcSetPositions(NetworkConnection target, int playerPosition, int[] opponentPositions)
+        {
+            playerImage.GetComponent<RectTransform>().anchoredPosition = playerPositionVectors[playerPosition];
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (i < opponentPositions.Length)
+                {
+                    opponentImages[i].GetComponent<RectTransform>().anchoredPosition = opponentPositionVectors[i][opponentPositions[i]];
+                }
+                else
+                {
+                    opponentImages[i].gameObject.SetActive(false);
                 }
             }
         }
