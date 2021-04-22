4ad0b94b36723325424036d1d35cd55566497151
Robert van Kooten <39274174+ThatRobVK@users.noreply.github.com>
2021-04-22 21:19:12 +0100

    Limit to 4 players total

--- Assets/Scripts/UI/Offline/HostButton.cs
+++ Assets/Scripts/UI/Offline/HostButton.cs
@@ -9,6 +9,7 @@ public class HostButton : MonoBehaviour
     }
     public void HandleButtonClick()
     {
+        NetworkManager.singleton.maxConnections = 4;
         NetworkManager.singleton.StartHost();
     }
 }
