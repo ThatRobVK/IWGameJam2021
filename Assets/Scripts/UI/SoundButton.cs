0975e2d408a51640d343426f7d57302f1ffd27e8
Robert van Kooten <39274174+ThatRobVK@users.noreply.github.com>
2021-04-23 21:03:36 +0100

    Add sound on/off button

---
+++ Assets/Scripts/UI/SoundButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite soundOnIcon;
+    [SerializeField]
+    private Sprite soundOffIcon;
+    [SerializeField]
+    private Image buttonImage;
+
+    private AudioSource[] audioSources;
+    private bool audioEnabled = true;
+
+    void Awake()
+    {
+        audioSources = FindObjectsOfType<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Reset position - it changes on clients for some weird reason
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(-98.69995f, 25.09998f);
+
+        buttonImage.sprite = (audioEnabled) ? soundOnIcon : soundOffIcon;
+    }
+
+    public void HandleButtonClick()
+    {
+        audioEnabled = !audioEnabled;
+
+        foreach (var audioSource in audioSources)
+        {
+            audioSource.enabled = audioEnabled;
+        }
+    }
+}
